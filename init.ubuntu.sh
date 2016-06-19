44e566a4056af433d27eb8c1fc3d87083670c3c8
huangxiang <rover0321@qq.com>
2016-06-19 15:20:25 +0800

    Keywords: 初始化 Description: 添加系统初始化脚本。 FileList: 	新文件：   README.md 	新文件：   init.ubuntu.sh

---
+++ init.ubuntu.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install openssh-server openssh-client git
+
+#init dirs
+cd
+mkdir wks
+mkdir ins
+mkdir sft
+
+#init git config
+git config --global user.email="rover0321@qq.com"
+git config --global user.name="huangxiang"
+
+echo 'git clone https://github.com/rover0321/ini.git'
+echo -n 'yes or no'
+read cmd
+git clone https://github.com/rover0321/ini.git
+
+echo 'git clone https://github.com/rover0321/bin.git'
+echo -n 'yes or no'
+read cmd
+git clone https://github.com/rover0321/bin.git
+
+mv .bashrc .bashrc.ori
+ln -s ini/bashrc .bashrc
+
+mv .profile .profile.ori
+ln -s ini/profile .profile
+
+mv .gitconfig .gitconfig.ori
+ln -s ini/gitconfig .gitconfig
+
+. .bashrc
