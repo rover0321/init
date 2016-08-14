0a1982c33bd860e1f78b3b0722f07b49b355a62d
huangxiang <rover0321@qq.com>
2016-08-14 15:54:52 +0800

    Keywords: init Description: Update init sketch file to add some prompts. FileList: 	修改：     init.ubuntu.sh

--- init.ubuntu.sh
+++ init.ubuntu.sh
@@ -14,22 +14,22 @@ git config --global user.email "rover0321@qq.com"
 git config --global user.name "huangxiang"
 
 echo 'git clone https://github.com/rover0321/ini.git'
-echo -n 'yes or no'
-read cmd
 git clone https://github.com/rover0321/ini.git
 
 echo 'git clone https://github.com/rover0321/bin.git'
-echo -n 'yes or no'
-read cmd
 git clone https://github.com/rover0321/bin.git
 
+echo 'back up old .bashrc file.'
 mv .bashrc .bashrc.ori
 ln -s ini/bashrc .bashrc
 
+echo 'back up old .profile file.'
 mv .profile .profile.ori
 ln -s ini/profile .profile
 
+echo 'back up old .gitconfig file.'
 mv .gitconfig .gitconfig.ori
 ln -s ini/gitconfig .gitconfig
 
+echo 'launch new .bashrc file.'
 . .bashrc
