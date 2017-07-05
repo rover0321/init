2808b205a8281b5b9c8a1da502e4e23cd637a374
huangxiang <rover0321@qq.com>
2017-07-05 16:43:50 +0800

    Description: Update init.sh to add download tmc mvn and jdk.

--- init.ubuntu.sh
+++ init.ubuntu.sh
@@ -41,38 +41,76 @@ if [ $cmd == 'y' ];then
     git clone https://github.com/rover0321/bin.git
 fi
 
-os=`uname`
-if [ -f .bashrc ];then
-    echo 'back up old .bashrc file.'
-    mv .bashrc .bashrc.ori
-fi
-if [ $os == 'Darwin' ];then
-    ln -s .bashrc .bash_profile
-else
-    ln -s ini/bashrc .bashrc
+# begin download maven tomcat jdk
+# switch to sft
+cd ~/sft
+
+# download and config maven
+read -p "Would you like download maven for you? y/n " cmd
+if [ $cmd == 'y' ];then
+    echo 'wget http://mirror.bit.edu.cn/apache/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.tar.gz'
+    wget http://mirror.bit.edu.cn/apache/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.tar.gz
+    tar -xzvf apache-maven-3.5.0-bin.tar.gz
+    ln -s ~/sft/apache-maven-3.5.0 ~/ins/mvn
 fi
 
-if [ -f .profile ];then
-    echo 'back up old .profile file.'
-    mv .profile .profile.ori
+# download and config jdk
+read -p "Would you like download jdk for you? y/n " cmd
+if [ $cmd == 'y' ];then
+    wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz
+    tar -xzvf jdk-8u131-linux-x64.tar.gz
+    ln -s ~/sft/jdk1.8.0_131 ~/ins/jdk
 fi
-ln -s ini/profile .profile
 
-if [ -f .vimrc ];then
-    echo 'Back up old .vimrc file.'
-    mv .vimrc .vimrc.ori
+# download and config tomcat
+read -p "Would you like download tomcat for you? y/n " cmd
+if [ $cmd == 'y' ];then
+    wget http://mirrors.tuna.tsinghua.edu.cn/apache/tomcat/tomcat-8/v8.5.16/bin/apache-tomcat-8.5.16.tar.gz
+    tar -xzvf apache-tomcat-8.5.16.tar.gz
+    ln -s ~/sft/apache-tomcat-8.5.16 ~/ins/tmc
 fi
-ln -s ini/vimrc .vimrc
 
-if [ -f .gitignore ];then
-    read -p "Would you like to add custom .gitignore file? y/n " cmd
-    if [ $cmd == 'y' ];then
-        echo 'back up old .gitignore file.'
-        mv .gitignore .gitignore.ori
-        ln -s ini/gitignore .gitignore
+# back to home directory
+cd ~
+
+read -p "Would you like config .bashrc and .profile for you? y/n " cmd
+if [ $cmd == 'y' ]; then
+    os=`uname`
+    if [ -f .bashrc ];then
+        echo 'back up old .bashrc file.'
+        mv .bashrc .bashrc.ori
+    fi
+    if [ $os == 'Darwin' ];then
+        ln -s .bashrc .bash_profile
+    else
+        ln -s ini/bashrc .bashrc
+    fi
+
+    if [ -f .profile ];then
+        echo 'back up old .profile file.'
+        mv .profile .profile.ori
+    fi
+    ln -s ini/profile .profile
+
+    if [ -f .vimrc ];then
+        echo 'Back up old .vimrc file.'
+        mv .vimrc .vimrc.ori
+    fi
+    ln -s ini/vimrc .vimrc
+fi
+
+read -p "Would you like config .gitignore for you? y/n " cmd
+if [ $cmd == 'y' ]; then
+    if [ -f .gitignore ];then
+        read -p "Would you like to add custom .gitignore file? y/n " cmd
+        if [ $cmd == 'y' ];then
+            echo 'back up old .gitignore file.'
+            mv .gitignore .gitignore.ori
+            ln -s ini/gitignore .gitignore
+        fi
     fi
 fi
 echo 'Launch new .bashrc file.'
 source .bashrc
 
-echo 'Exit script.'
+echo 'Exit init.sh script.'
