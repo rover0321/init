78a684776abe3e7774606af3c49d35853aec86c6
huangxiang <rover0321@qq.com>
2018-08-08 19:58:45 +0800

    Description: Update installation of cmake

--- init.ubuntu.sh
+++ init.ubuntu.sh
@@ -107,7 +107,7 @@ fi
 line
 read -p "Would you like install cmake? y/n " cmd
 if [ $cmd == 'y' ]; then
-    wget https://cmake.org/files/v3.10/cmake-3.10.3-Linux-x86_64.tar.gz && tar -xzvf cmake-3.10.3-Linux-x86_64.tar.gz && ln -s ~/sft/cmake-3.10.3-Linux-x86_64 ~/ins/cmake
+    sudo apt-get install cmake
 fi
 
 line
